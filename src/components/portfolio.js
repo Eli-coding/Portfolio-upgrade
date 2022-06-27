a78df5fd70a1c70ee04d017f6d7a03f3809a394b
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-06-27 14:30:49 -0400

    portfolio/ contact in progress

---
+++ src/components/portfolio.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+export default function Portfolio(){
+
+    return(
+
+
+    <div className="row align-items-center">
+            
+                <div>
+                    <h1> MY WORK</h1>
+                </div>
+
+                
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://github.com/bxz5089/Cumulus-Chat/blob/main/public/assets/ScreenShot1.png?raw=true"height="350" width="350"
+                                class="card-img-top" alt="project-2"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Cumulus Cloud </h5>
+                                <a href="https://infinite-caverns-73704.herokuapp.com/" class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/bxz5089/Cumulus-Chat" class="btn btn-info">Github Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://user-images.githubusercontent.com/80432031/149646105-49ddc354-6ed8-4f72-a10a-8cbfdaeff5f8.png" height="350" width="350"
+                                class="card-img-top" alt="planner"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Day Planner</h5>
+                                <a href="https://eli-coding.github.io/Work-Day-Scheduler/"
+                                    class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/Eli-coding/Work-Day-Scheduler" class="btn btn-info">Github
+                                    Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://user-images.githubusercontent.com/80432031/156679972-2c173845-a36f-4544-b337-2cabb82bd1fb.png" height="350" width="350"
+                                class="card-img-top" alt="passwordG" />
+                            <div class="card-body">
+                                <h5 class="card-title">Note-Pad</h5>
+                                <a href="https://note-pad-ey.herokuapp.com/notes"
+                                    class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/Eli-coding/Note-Pad" class="btn btn-info">Github
+                                    Repo</a>
+                            </div>
+                   </div>
+                    </div>
+    
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://github.com/bxz5089/Cumulus-Chat/blob/main/public/assets/ScreenShot1.png?raw=true"height="350" width="350"
+                                class="card-img-top" alt="project-2"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Cumulus Cloud </h5>
+                                <a href="https://infinite-caverns-73704.herokuapp.com/" class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/bxz5089/Cumulus-Chat" class="btn btn-info">Github Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://user-images.githubusercontent.com/80432031/149646105-49ddc354-6ed8-4f72-a10a-8cbfdaeff5f8.png" height="350" width="350"
+                                class="card-img-top" alt="planner"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Day Planner</h5>
+                                <a href="https://eli-coding.github.io/Work-Day-Scheduler/"
+                                    class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/Eli-coding/Work-Day-Scheduler" class="btn btn-info">Github
+                                    Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://user-images.githubusercontent.com/80432031/156679972-2c173845-a36f-4544-b337-2cabb82bd1fb.png" height="350" width="350"
+                                class="card-img-top" alt="passwordG" />
+                            <div class="card-body">
+                                <h5 class="card-title">Note-Pad</h5>
+                                <a href="https://note-pad-ey.herokuapp.com/notes"
+                                    class="btn btn-info">Deployed</a>
+                                <a href="https://github.com/Eli-coding/Note-Pad" class="btn btn-info">Github
+                                    Repo</a>
+                            </div>
+                   </div>
+                    </div>       
+    </div> 
+
+
+
+
+
+
+
+    );
+}
